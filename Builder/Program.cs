d56f6c4ed445ba696ec21806921da4affca948f0
Neomaster <the@neomaster.net>
2022-02-24 03:56:52 +0300

    0.40.1.0 Номер версии и дата сборки на странице.

---
+++ Builder/Program.cs
@@ -0,0 +1,25 @@
+﻿using LibGit2Sharp;
+using System.Text.RegularExpressions;
+
+string solutionDirectory = new DirectoryInfo(Directory.GetCurrentDirectory()).Parent.FullName;
+string repoPath = Path.Combine(solutionDirectory, ".git");
+
+using (Repository repo = new Repository(repoPath))
+{
+    string version = Regex.Replace(repo.Head.Commits.First().Message, @"\s.*", "");
+    string datetimeString = DateTime.Now.ToString("yyyy.MM.dd HH:mm:ss");
+    string infoHtmlCode = $"<div id='version'>v{version}<br>{datetimeString}</div>";
+    string appComponentPath = Path.Combine(solutionDirectory, @"Client\src\app\app.component.html");
+    string appComponentText = File.ReadAllText(appComponentPath);
+
+    if (Regex.IsMatch(appComponentText, @"<div id='version'>"))
+    {
+        appComponentText = Regex.Replace(appComponentText, @"<div id='version'>.*", infoHtmlCode);
+    }
+    else
+    {
+        appComponentText += infoHtmlCode;
+    }
+    
+    File.WriteAllText(appComponentPath, appComponentText);
+}
